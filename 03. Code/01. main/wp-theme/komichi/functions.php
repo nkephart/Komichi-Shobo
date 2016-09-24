6471f6e28eb4dffe967389a988b9f437d8531dd5
Naoki Kephart <naoki.kephart@gmail.com>
2016-09-24 09:14:06 +0900

    Update

--- 03. Code/01. main/wp-theme/komichi/functions.php
+++ 03. Code/01. main/wp-theme/komichi/functions.php
@@ -353,3 +353,11 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Added on 2016/9/19
+ */
+function modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">続きを読む&nbsp;&rarr;</a>';
+}
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
--- 03. Code/01. main/wp-theme/komichi/header.php.bak.php
+++ 03. Code/01. main/wp-theme/komichi/header.php.bak.php
@@ -23,16 +23,4 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-<div id="content" class="site-content">
-<header role="banner" id="global-header" class="cf">
-  <h1><a href="/test" title="こみち書房">こみち書房</a></h1>
-  <nav role="navigation" class="header-navigation">
-    <ol>
-      <li><a href="/test/news" class="nav-news" title="お知らせ"></a></li>
-      <li><a href="/test/books" class="nav-books" title="書籍の紹介"></a></li>
-      <li><a href="/test/bs" class="nav-bs" title="書店さまへ"></a></li>
-      <li><a href="/test/order" class="nav-order" title="本を購入される方へ"></a></li>
-      <li><a href="/test/company" class="nav-company" title="会社について"></a></li>
-    </ol>
-  </nav>
-</header>
+	<div id="content" class="site-content">
